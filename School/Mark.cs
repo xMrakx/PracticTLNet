594f897541e52637bcd3418c4e26af9b26dccfc9
Mrak <mrakobes1505@gmail.com>
2022-06-28 13:48:49 +0300

    Добавьте файлы проекта.

---
+++ School/Mark.cs
@@ -0,0 +1,19 @@
+﻿namespace School;
+
+public class Mark
+{
+    public Mark(int value, string className, DateTime date)
+    {
+        if (value < 0 || value > 5)
+        {
+            throw new ArgumentOutOfRangeException("invalid mark value");
+        }
+        Value = value;
+        ClassName = className;
+        Date = date;
+    }
+
+    public int Value { get; private set; }
+    public string ClassName { get; private set; }
+    public DateTime Date { get; private set; }
+}
